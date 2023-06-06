b93a7ea9bdb8d7a04680ce6bd6731f43b3d0f4c7
Akshat143 <akshat.github@gmail.com>
2023-06-07 04:16:42 +0530

    implemented delete functionality

--- src/Orders.js
+++ src/Orders.js
@@ -1,13 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
+  const [deleteOrderId, setDeleteOrderId] = useState(null);
 
   useEffect(() => {
-    fetch('https://631945908e51a64d2be10770.mockapi.io/api/v1/allOrders')
-      .then(response => response.json())
-      .then(data => setOrders(data));
+    fetchOrders();
   }, []);
+
+  const fetchOrders = async () => {
+    try {
+      const response = await fetch(
+        "https://631945908e51a64d2be10770.mockapi.io/api/v1/allOrders"
+      );
+      const data = await response.json();
+      setOrders(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDelete = (id) => {
+    setDeleteOrderId(id);
+  };
+
+  const confirmDelete = async () => {
+    try {
+      const updatedOrders = orders.filter((order) => order.id !== deleteOrderId);
+      setOrders(updatedOrders);
+      setDeleteOrderId(null);
+
+      await fetch(
+        `https://631945908e51a64d2be10770.mockapi.io/api/v1/allOrders/${deleteOrderId}`,
+        {
+          method: "DELETE",
+        }
+      );
+
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const cancelDelete = () => {
+    setDeleteOrderId(null);
+  };
 
   return (
     <div>
@@ -24,18 +62,40 @@ const Orders = () => {
           </tr>
         </thead>
         <tbody>
-          {orders.map(order => (
+          {orders.map((order) => (
             <tr key={order.id}>
               <td>{order.id}</td>
               <td>{order.orderDescription}</td>
               <td>
-                Apparel: {order.countOfItemTypes?.apparel ?? 0} | Grocery: {order.countOfItemTypes?.grocery ?? 0}
+                Apparel: {order.countOfItemTypes?.apparel ?? 0} Grocery:{" "}
+                {order.countOfItemTypes?.grocery ?? 0}
               </td>
-              <td>{Math.round((order.countOfItemTypes?.apparel / (order.countOfItemTypes?.apparel + order.countOfItemTypes?.grocery)) * 100)}%</td>
+              <td>
+                {order.countOfItemTypes?.apparel !== 0 &&
+                (order.countOfItemTypes?.apparel ||
+                  order.countOfItemTypes?.grocery)
+                  ? `${Math.round(
+                      (order.countOfItemTypes?.apparel /
+                        (order.countOfItemTypes?.apparel +
+                          order.countOfItemTypes?.grocery)) *
+                        100
+                    )}%`
+                  : "0%"}
+              </td>
               <td>{order.createdBy}</td>
               <td>{order.createdAt}</td>
               <td>
-                <button>Delete</button>
+                {deleteOrderId === order.id ? (
+                  <>
+                    <span>
+                      Are you sure you want to delete Order ID {order.id}?
+                    </span>
+                    &nbsp;<button onClick={confirmDelete}>Yes</button>
+                    &nbsp;<button onClick={cancelDelete}>No</button>
+                  </>
+                ) : (
+                  <button onClick={() => handleDelete(order.id)}>Delete</button>
+                )}
               </td>
             </tr>
           ))}
